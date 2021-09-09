7837da83aa5b5be24b911af767087770eda79709
Matt Carlson <mjcarlson628@gmail.com>
2021-09-09 16:11:34 -0400

    demographic infobox

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,6 @@
 console.log(data);
 let samples = data.samples;
+let metadata = data.metadata;
 let id = 940;
 
 function init() {
@@ -10,8 +11,7 @@ function init() {
     for (let i = 0; i < 10; i++) {
         initLabels.push(`OTU ${data.samples[0].otu_ids[i]}`);
     };
-
-
+// Initial bar chart of top 10 otu ids
     let initBar = [{
         x: sampleInit.slice(0, 10).reverse(),
         y: initLabels.reverse(),
@@ -19,7 +19,7 @@ function init() {
         type: "bar",
         orientation: "h"
     }];
-
+// Initial bubble chart
     let initBubble = [{
         x: otuidInit,
         y: sampleInit,
@@ -27,27 +27,33 @@ function init() {
         opacity: 0.75,
         marker: {
             color: otuidInit,
+            colorscale: 'Jet',
             size: sampleInit.map(item => item * 25),
             sizemode: "area"
         },
     }];
-
     let bubbleLayout = {
         xaxis: {
             title: {text: "OTU ID"}
         }
     };
+// Dropdown menu to select id number
+    let dropdownMenu = d3.select("select");
+    for (let m = 0; m < data.names.length; m++) {
+        dropdownMenu.append("option").text(data.names[m]);
+    };
+// Demographic info
+    let demographic = d3.select(".panel-body");
+    for (let d = 0; d < Object.keys(metadata[0]).length; d++) {
+        demographic.append("p").text(`${Object.keys(metadata[0])[d]}: ${Object.values(metadata[0])[d]}`)
+    };
 
+// Create the charts
     Plotly.newPlot("bar", initBar);
     Plotly.newPlot("bubble", initBubble, bubbleLayout);
 };
 
-// Dropdown menu to select ID number
-let dropdownMenu = d3.select("select");
-for (let m = 0; m < data.names.length; m++) {
-    dropdownMenu.append("option").text(data.names[m]);
-};
-
+// Restyle charts for new id by clicking a dropdown option
 d3.selectAll("#selDataset").on("click", optionChanged);
 
 function optionChanged() {
@@ -56,51 +62,41 @@ function optionChanged() {
     let otuIds = [];
     let otuBars = [];
     let otuLabels = [];
-
-    console.log(id);
-
-// Horizontal bar chart
-// Match metadata id with samples id
+    let newDemographic = [];
+// Demographic info for new id
     for (let i = 0; i < data.metadata.length; i++) {
         if (data.metadata[i].id==id) {
-            // console.log(data.metadata[i].age);
+            let demographic = d3.selectAll(".panel-body");
+            demographic.text("");
+            for (let d = 0; d < Object.keys(metadata[i]).length; d++) {
+                demographic.append("p").text(`${Object.keys(metadata[i])[d]}: ${Object.values(metadata[i])[d]}`)
+            };
+// Match metadata id with samples id
             for (let j = 0; j < data.samples.length; j++) {
                 if (data.samples[j].id==id) {
-                    // console.log(data.samples[i].otu_ids[0]);
-// Arrays of first 10 otu_ids, sample_values, and otu_labels
-                    for (let k = 0; k < 10; k++) {
+// Horizontal bar chart
+// Arrays of otu_ids, sample_values, and otu_labels
+                    for (let k = 0; k < samples[j].sample_values.length; k++) {
                         sampleValues.push(data.samples[j].sample_values[k]);
                         otuIds.push(data.samples[j].otu_ids[k]);
                         otuBars.push(`OTU ${data.samples[j].otu_ids[k]}`);
                         otuLabels.push(data.samples[j].otu_labels[k]);
                     };
-// Bubble Chart
-                    let bubbleTrace = [{
-                        x: otuIds,
-                        y: sampleValues,
-                        text: otuLabels,
-                        mode: "markers",
-                        marker: {
-                            // color: ,
-                            size: Math.sqrt(sampleValues) * 2
-                        }
-                    }];
+                    console.log(sampleValues);
+// Plot the bar chart of top 10 otus sorted descending by sample_values
+                    Plotly.restyle("bar", "x", [sampleValues.slice(0, 10).reverse()]);
+                    Plotly.restyle("bar", "y", [otuBars.slice(0, 10).reverse()]);
+                    Plotly.restyle("bar", "text", [otuLabels.slice(0, 10).reverse()]);
+// Restyle bubble chart to match new id number
+                    Plotly.restyle("bubble", "x", [data.samples[j].otu_ids]);
+                    Plotly.restyle("bubble", "y", [data.samples[j].sample_values]);
+                    Plotly.restyle("bubble", "marker.size", [data.samples[j].sample_values.map(item => item * 25)]);
+                    Plotly.restyle("bubble", "marker.color", [data.samples[j].otu_ids]);
                 };
             };
-            console.log(sampleValues);
-// Plot the bar chart sorted descending by sample_values
-            Plotly.restyle("bar", "x", [sampleValues.reverse()]);
-            Plotly.restyle("bar", "y", [otuBars.reverse()]);
-            Plotly.restyle("bar", "text", [otuLabels.reverse()]);
-// Restyle bubble chart
-            Plotly.restyle("bubble", "x", [otuIds.reverse()]);
-            Plotly.restyle("bubble", "y", [sampleValues.reverse]);
-            Plotly.restyle("bubble", "color", [otuIds.reverse]);
-            };
-
         };
-
     };
+};
 
 
 init();
